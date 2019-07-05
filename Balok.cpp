3b9a09b79d39fcbc77c43ba16f918bfa94e14f65
ekaindrajaya778 <49395680+ekaindrajaya778@users.noreply.github.com>
2019-07-05 17:41:53 +0800

    Add files via upload

---
+++ Balok.cpp
@@ -0,0 +1,13 @@
+#include <iostream.h>
+#include <conio.h>
+int main ()
+{
+float volume, panjang, lebar, tinggi ;
+cout<<"masukkan panjang balok : "; cin >>panjang;
+cout<<"masukkan lebar balok : "; cin >>lebar;
+cout<<"masukkan tinggi balok : "; cin >>tinggi;
+volume = panjang*lebar*tinggi;
+cout<<"volume balok = "<<volume<<"cm"<<endl;
+getch();
+}
+
---
+++ Jari-jari Bola.cpp
@@ -0,0 +1,9 @@
+#include <iostream.h>
+#include <conio.h>
+int main () {
+float volume, r;
+cout<<"masukkan jari - jari bola : "; cin >>r;
+volume = 4/3*3.14*r*r*r;
+cout<<"volume bola = "<<volume<<"cm"<<endl;
+getch ();
+}
